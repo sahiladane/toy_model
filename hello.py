3958f684e7cce817988149ac32bb8de83bf4ac26
sahiladane <sahiladane@gmail.com>
2024-04-18 11:33:24 -0500

    Sam's script with long run divergence plots

--- hello.py
+++ hello.py
@@ -24,48 +24,85 @@ I also update the prior of Alice and Bob on how truthful Fox is and they still k
 
 '''
 
+import random as r
+
 prior_alice = 0.5
 prior_bob = 0.5
 
-fox_truthful_alice = 1
-fox_truthful_bob = 0.8
+fox_truthful_alice = 0.4
+fox_truthful_bob = 0.6
 
-left_given_truthful_left = 0.9
-left_given_truthful_right = 0.1
+def bayes_updating_l(prior_alice, prior_bob, fox_truthful_alice, fox_truthful_bob):
 
-left_given_lies_left = 0
-left_given_lies_right = 0
+        left_given_truthful_left = 0.7
+        left_given_truthful_right = 0.3
+
+        left_given_lies_left = 0.3
+        left_given_lies_right = 0.7
+
+        # posterior after observing left 
+
+        posterior_alice_l = prior_alice * ( (1 - fox_truthful_alice) * left_given_lies_right + fox_truthful_alice * left_given_truthful_right)/ ( prior_alice * ( (1 - fox_truthful_alice) * left_given_lies_right + fox_truthful_alice * left_given_truthful_right) + (1-prior_alice) * ( (1 - fox_truthful_alice) * left_given_lies_left + fox_truthful_alice * left_given_truthful_left) )
+
+        #print(posterior_alice_l)
+
+        posterior_bob_l = prior_bob * ( (1 - fox_truthful_bob) * left_given_lies_right + fox_truthful_bob * left_given_truthful_right)/ ( prior_bob * ( (1 - fox_truthful_bob) * left_given_lies_right + fox_truthful_bob * left_given_truthful_right) + (1-prior_bob) * ( (1 - fox_truthful_bob) * left_given_lies_left + fox_truthful_bob * left_given_truthful_left) )
+
+        #print(posterior_bob_l)
+
+        fox_posterior_alice_l = fox_truthful_alice * ( prior_alice * left_given_truthful_right + (1-prior_alice) * left_given_truthful_left) / (fox_truthful_alice * ( prior_alice * left_given_truthful_right + (1-prior_alice) * left_given_truthful_left) + (1-fox_truthful_alice) * ( prior_alice * left_given_lies_right + (1-prior_alice) * left_given_lies_left) )
+
+        #print(fox_posterior_alice_l)
+
+        fox_posterior_bob_l = fox_truthful_bob * ( prior_bob * left_given_truthful_right + (1-prior_bob) * left_given_truthful_left) / (fox_truthful_bob * ( prior_bob * left_given_truthful_right + (1-prior_bob) * left_given_truthful_left) + (1-fox_truthful_bob) * ( prior_bob * left_given_lies_right + (1-prior_bob) * left_given_lies_left) )
+
+        #print(fox_posterior_bob_l,'\n')
+
+        return posterior_alice_l, posterior_bob_l, fox_posterior_alice_l, fox_posterior_bob_l
+
+def bayes_updating_r(prior_alice, prior_bob, fox_truthful_alice, fox_truthful_bob):
+
+        left_given_truthful_left = 0.7
+        left_given_truthful_right = 0.3
+
+        left_given_lies_left = 0.3
+        left_given_lies_right = 0.7
+
+        # posteriors after observing right
+
+        posterior_alice_r = prior_alice * ((1 - fox_truthful_alice) * (1-left_given_lies_right) + fox_truthful_alice * (1-left_given_truthful_right))/ ( prior_alice * ( (1 - fox_truthful_alice) * (1-left_given_lies_right) + fox_truthful_alice * (1-left_given_truthful_right)) + (1-prior_alice) * ( (1 - fox_truthful_alice) * (1-left_given_lies_left) + fox_truthful_alice * (1-left_given_truthful_left)) )
+
+        #print(posterior_alice_r)
+
+        posterior_bob_r = prior_bob * ( (1 - fox_truthful_bob) * (1-left_given_lies_right) + fox_truthful_bob * (1-left_given_truthful_right))/ ( prior_bob * ( (1 - fox_truthful_bob) * (1-left_given_lies_right) + fox_truthful_bob * (1-left_given_truthful_right)) + (1-prior_bob) * ( (1 - fox_truthful_bob) * (1-left_given_lies_left) + fox_truthful_bob * (1-left_given_truthful_left)) )
+
+        #print(posterior_bob_r)
+
+        fox_posterior_alice_r = fox_truthful_alice * ( prior_alice * (1-left_given_truthful_right) + (1-prior_alice) * (1-left_given_truthful_left)) / (fox_truthful_alice * ( prior_alice * (1-left_given_truthful_right) + (1-prior_alice) * (1-left_given_truthful_left)) + (1-fox_truthful_alice) * ( prior_alice * (1-left_given_lies_right) + (1-prior_alice) * (1-left_given_lies_left)) )
+
+        #print(fox_posterior_alice_r)
+
+        fox_posterior_bob_r = fox_truthful_bob * ( prior_bob * (1-left_given_truthful_right) + (1-prior_bob) * (1-left_given_truthful_left)) / (fox_truthful_bob * ( prior_bob * (1-left_given_truthful_right) + (1-prior_bob) * (1-left_given_truthful_left)) + (1-fox_truthful_bob) * ( prior_bob * (1-left_given_lies_right) + (1-prior_bob) * (1-left_given_lies_left)) )
+
+        #print(fox_posterior_bob_r,'\n')
+
+        return posterior_alice_r, posterior_bob_r, fox_posterior_alice_r, fox_posterior_bob_r
 
 
-def bayes_updating(prior_alice, prior_bob, fox_truthful_alice, fox_truthful_bob):
+for i in range(10000):
+        # p= probability of observing r
+        ex = r.binomialvariate(n=1,p=0.7)
+        if ex == 1:
+                a, b, c, d = bayes_updating_r(prior_alice, prior_bob, fox_truthful_alice, fox_truthful_bob)
+                prior_alice, prior_bob, fox_truthful_alice, fox_truthful_bob = a, b, c, d
+        else:
+                a, b, c, d = bayes_updating_l(prior_alice, prior_bob, fox_truthful_alice, fox_truthful_bob)
+                prior_alice, prior_bob, fox_truthful_alice, fox_truthful_bob = a, b, c, d
 
-	# posterior after observing left 
-
-	posterior_alice = prior_alice * ( (1 - fox_truthful_alice) * left_given_lies_right + fox_truthful_alice * left_given_truthful_right)/ ( prior_alice * ( (1 - fox_truthful_alice) * left_given_lies_right + fox_truthful_alice * left_given_truthful_right) + (1-prior_alice) * ( (1 - fox_truthful_alice) * left_given_lies_left + fox_truthful_alice * left_given_truthful_left) )
-
-	print(posterior_alice)
-
-	posterior_bob = prior_bob * ( (1 - fox_truthful_bob) * left_given_lies_right + fox_truthful_bob * left_given_truthful_right)/ ( prior_bob * ( (1 - fox_truthful_bob) * left_given_lies_right + fox_truthful_bob * left_given_truthful_right) + (1-prior_bob) * ( (1 - fox_truthful_bob) * left_given_lies_left + fox_truthful_bob * left_given_truthful_left) )
-
-	print(posterior_bob)
-
-	fox_posterior_alice = fox_truthful_alice * ( prior_alice * left_given_truthful_right + (1-prior_alice) * left_given_truthful_left) / (fox_truthful_alice * ( prior_alice * left_given_truthful_right + (1-prior_alice) * left_given_truthful_left) + (1-fox_truthful_alice) * ( prior_alice * left_given_lies_right + (1-prior_alice) * left_given_lies_left) )
-
-	print(fox_posterior_alice)
-
-	fox_posterior_bob = fox_truthful_bob * ( prior_bob * left_given_truthful_right + (1-prior_bob) * left_given_truthful_left) / (fox_truthful_bob * ( prior_bob * left_given_truthful_right + (1-prior_bob) * left_given_truthful_left) + (1-fox_truthful_bob) * ( prior_bob * left_given_lies_right + (1-prior_bob) * left_given_lies_left) )
-
-	print(fox_posterior_bob,'\n')
-
-	return posterior_alice, posterior_bob, fox_posterior_alice, fox_posterior_bob
+print(prior_alice)
+print(fox_truthful_alice)
+print(prior_bob)
+print(fox_truthful_bob)
 
 
-for i in range(10):
-	a, b, c, d = bayes_updating(prior_alice, prior_bob, fox_truthful_alice, fox_truthful_bob)
-	prior_alice, prior_bob, fox_truthful_alice, fox_truthful_bob = a, b, c, d
 
-
-# who updates more? 
-
-
-# https://www.desmos.com/calculator/fbxxmfq4bm
